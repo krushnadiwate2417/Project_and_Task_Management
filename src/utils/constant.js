f2b188ce48b13cc7ba67f88889026560e15f61bb
krushnadiwate2417 <krushnadiwate2417@gmail.com>
2025-04-17 19:25:28 +0530

    Created Apis for desired funcationality and added to constants

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,3 +1,14 @@
 export const ADMIN_IMG = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThpUgC7dkHYV0KD26Ujw5u83EI43dOZqvABg&s';
+export const USER_IMG = 'https://static.vecteezy.com/system/resources/thumbnails/000/550/980/small/user_icon_001.jpg';
 
-export const USER_IMG = 'https://static.vecteezy.com/system/resources/thumbnails/000/550/980/small/user_icon_001.jpg'
+export const GET_ALL_USERS = 'https://project-management-server-five.vercel.app/api/v1/users/users';
+export const SIGN_UP_USER = 'https://project-management-server-five.vercel.app/api/v1/users/signUp';
+export const LOGIN_USER = 'https://project-management-server-five.vercel.app/api/v1/users/login';
+
+export const ADD_PROJECT = 'https://project-management-server-five.vercel.app/api/v1/project/addProject';
+export const GET_PROJECTS = 'https://project-management-server-five.vercel.app/api/v1/project/getProjects';
+
+export const ADD_TASK = 'https://project-management-server-five.vercel.app/api/v1/task/addTask';
+export const GET_ALL_TASK = 'https://project-management-server-five.vercel.app/api/v1/task/showAllTasks';
+export const GET_A_TASK = 'https://project-management-server-five.vercel.app/api/v1/task/showTask/:taskId';
+export const ADD_A_COMMENT = 'https://project-management-server-five.vercel.app/api/v1/task/addComment';
